0365f9dfa5dbfcd86bcb52f891f48897b85c7f43
ErinFinnegan <eef206@nyu.edu>
2014-04-19 15:49:45 -0400

    got the guys within the open  mouth to change colors

--- SecondAttempt/vehicle.js
+++ SecondAttempt/vehicle.js
@@ -98,10 +98,14 @@
 	  this.position.add(this.velocity);
 	  // Reset accelertion to 0 each cycle
 	  this.acceleration.mult(0);
-	    if(mouthopen === true){
-	    	myp5.fill(255, 0, 0); 
-	    	console.log("mouthX, bottomlip " + mouthX + ", " + bottomlip);
-	    } 
+	     if(mouthopen === true && (this.position.y < (bottomlip + 10)) && (this.position.y > (bottomlip-10))) {
+	   //	myp5.fill(255, 0, 0); 
+	   // 	console.log("positionx vs mouthX" + this.position.x + ", " + mouthX);
+	     	if ((this.position.x <= (mouthX + 10)) && (this.position.x >= (mouthX - 10))){
+	    		 myp5.fill(0,255,0);
+	    	//console.log("positionx vs mouthX" + this.position.x + ", " + mouthX);
+		    }
+	    }
 	    	else {
 			  myp5.fill(0, 0, 255);
 		    	}
