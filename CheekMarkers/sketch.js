aa52af5fb7df0e64829dceb5bf7bfbc0e02d42e3
ErinFinnegan <eef206@nyu.edu>
2014-04-26 16:44:07 -0400

    changed folder name

---
+++ CheekMarkers/sketch.js
@@ -0,0 +1,41 @@
+// The Nature of Code, Daniel Shiffman http://natureofcode.com
+//adapting example 6_08
+
+  var s = function( sketch ) {
+
+      var videostarted = false;
+      var vehicles = [];
+
+  sketch.setup = function() {
+
+      sketch.marshmallowghost = sketch.loadImage("styles/marshmallowghost.png");
+      canvas = sketch.createCanvas(400, 300);
+      canvas.class("p5canvas");   //references the HTML
+        // We are now making random vehicles and storing them in an array
+        for (var i = 0; i < 20; i++) {
+          vehicles.push(new Vehicle(350,250));
+        }
+      console.log("vehicles.length  = " + vehicles.length);
+      }
+
+   sketch.draw = function() {
+    sketch.clear();
+         myp5.fill(250,25,120);
+         myp5.noStroke;
+         myp5.ellipse((LeftsideOfmouthX-15), (LeftsideOfmouthY-15), 20, 20);
+         myp5.ellipse((RightsideOfmouthX+15), (RightsideOfmouthY-15), 20, 20);
+
+        
+        for (var i = 0; i < vehicles.length; i++) {
+          //vehicles[i].checkAlive() === true;
+          vehicles[i].applyBehaviors(vehicles);
+          vehicles[i].update();
+          vehicles[i].borders();
+          vehicles[i].display(); 
+          
+         
+        }
+      }
+ };
+
+var myp5 = new p5(s, 'p5canvas');
