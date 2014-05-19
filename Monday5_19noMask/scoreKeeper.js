674e78b5f0a1d67eea425adbd9065d43aa6c752a
ErinFinnegan <eef206@nyu.edu>
2014-05-19 15:00:49 -0400

    renamed files and duplicated files

---
+++ Monday5_19noMask/scoreKeeper.js
@@ -0,0 +1,32 @@
+
+
+//The following example is from http://diveintohtml5.info/storage.html
+//Check for local storage in your browser here:  http://diveintohtml5.info/detect.html#storage
+
+
+
+// interface Storage {
+//   getter any getItem(in DOMString key);
+//   setter creator void setItem(in DOMString key, in any data);
+// };
+
+// var highScore = localStorage["50"];
+// localStorage["50"] = highScore;
+
+
+
+// function scoreKeeper() {
+//     // if (!supportsLocalStorage()) { return false; }
+//     localStorage["highScore"] = myp5.score;
+//     console.log("scoreKeeper says highScore = " + highScore);
+//  //   for (var i = 0; i < kNumPieces; i++) {
+// 	// localStorage["halma.piece." + i + ".row"] = gPieces[i].row;
+// 	// localStorage["halma.piece." + i + ".column"] = gPieces[i].column;
+//  //    }
+//  //    localStorage["halma.selectedpiece"] = gSelectedPieceIndex;
+//  //    localStorage["halma.selectedpiecehasmoved"] = gSelectedPieceHasMoved;
+//  //    localStorage["halma.movecount"] = gMoveCount;
+//     return true;
+// }
+
+// scoreKeeper();
